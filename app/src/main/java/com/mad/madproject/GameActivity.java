bb7a09bb8be24419de29765465db18a90c251fb2
BananaCHIU <a463028938@gmail.com>
2019-04-17 12:48:56 +0800

    Game

--- app/src/main/java/com/mad/madproject/GameActivity.java
+++ app/src/main/java/com/mad/madproject/GameActivity.java
@@ -1,7 +1,9 @@
 package com.mad.madproject;
 
+import android.graphics.Point;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.View;
 
 public class GameActivity extends AppCompatActivity {
@@ -11,7 +13,33 @@ public class GameActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Get a Display object to access screen details
+        Display display = getWindowManager().getDefaultDisplay();
+        // Load the resolution into a Point object
+        Point size = new Point();
+        display.getSize(size);
+
+        // Initialize gameView and set it as the view
+        spaceInvadersView = new SpaceInvadersView(this, size.x, size.y);
         setContentView(R.layout.activity_game);
+    }
+
+    // This method executes when the player starts the game
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Tell the gameView resume method to execute
+        spaceInvadersView.resume();
+    }
+
+    // This method executes when the player quits the game
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // Tell the gameView pause method to execute
+        spaceInvadersView.pause();
     }
 
 
